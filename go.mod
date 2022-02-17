6e354033c01ca0ebaf87bd17c01f8c81d7b93f50
Patryk Matyjasek <patryk.matyjasek@gmail.com>
2022-02-17 21:54:09 +0100

    Add redis

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module patrykmatyjasek/distributed-tracing
 go 1.15
 
 require (
+	github.com/go-redis/redis/extra/redisotel v0.3.0
+	github.com/go-redis/redis/v8 v8.11.4
 	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace v0.26.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.25.0
 	go.opentelemetry.io/otel v1.1.0
 	go.opentelemetry.io/otel/exporters/jaeger v1.1.0
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.0.1
 	go.opentelemetry.io/otel/sdk v1.1.0
 	go.opentelemetry.io/otel/trace v1.1.0
 )
--- go/main.go
+++ go/main.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"net/http/httptrace"
 
+	"github.com/go-redis/redis/extra/redisotel"
+	"github.com/go-redis/redis/v8"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 
@@ -62,7 +64,7 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	span := trace.SpanFromContext(ctx)
 	defer span.End()
 	span.AddEvent("handling this request")
-	url := "https://www.wp.pl/"
+	url := "https://jsonplaceholder.typicode.com/posts/1/"
 	client := http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
 
 	_, _ = io.WriteString(w, "Hello, from Go!\n")
@@ -77,5 +79,9 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	}
 	_, err = ioutil.ReadAll(res.Body)
 	_ = res.Body.Close()
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:5672",
+	})
+	redisClient.AddHook(redisotel.TracingHook{})
 
 }
